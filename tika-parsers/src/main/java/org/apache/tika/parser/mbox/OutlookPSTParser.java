d268e22654604cf7289ebdb415caa3f4414d9730
Ben Thompson <ben.thompson@imanage.com>
2017-12-27 16:30:30 +0000

    OutlookPst Parser: Merge latest changes + change the way we parse the content
    Use the html body if it's present, otherwise use the plain text body.

--- tika-parsers/src/main/java/org/apache/tika/parser/mbox/OutlookPSTParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/mbox/OutlookPSTParser.java
@@ -21,7 +21,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.singleton;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
@@ -131,7 +130,7 @@ public class OutlookPSTParser extends AbstractParser {
                 //RecursiveParserWrapper copies the metadata and thereby prevents
                 //modifications to mailMetadata from making it into the
                 //metadata objects cached by the RecursiveParserWrapper
-                parseMailAttachments(handler, pstMail, mailMetadata, embeddedExtractor);
+                parseMailAttachments(handler, pstMail, mailMetadata, embeddedExtractor, pstMail.getInternetMessageId());
                 parserMailItem(handler, pstMail, mailMetadata, embeddedExtractor);
 
                 handler.endElement("div");
@@ -218,14 +217,22 @@ public class OutlookPSTParser extends AbstractParser {
         //that causes the HTMLParser to get the encoding wrong.  Better if we could get
         //the underlying bytes from the pstMail object...
 
-        byte[] mailContent = pstMail.getBody().getBytes(UTF_8);
-        mailMetadata.set(TikaCoreProperties.CONTENT_TYPE_OVERRIDE, MediaType.TEXT_PLAIN.toString());
-        embeddedExtractor.parseEmbedded(new ByteArrayInputStream(mailContent), handler, mailMetadata, true);
+        byte[] htmlContent = pstMail.getBodyHTML().getBytes(UTF_8);
+        byte[] plainContent = pstMail.getBody().getBytes(UTF_8);
+
+        //mailMetadata.set(TikaCoreProperties.CONTENT_TYPE_OVERRIDE, MediaType.TEXT_PLAIN.toString());
+        if (htmlContent.length > 0) {
+            embeddedExtractor.parseEmbedded(new ByteArrayInputStream(htmlContent), handler, mailMetadata, true);
+        } else {
+            mailMetadata.set(TikaCoreProperties.CONTENT_TYPE_OVERRIDE, MediaType.TEXT_PLAIN.toString());
+        embeddedExtractor.parseEmbedded(new ByteArrayInputStream(plainContent), handler, mailMetadata, true);
+        }
     }
 
     private void parseMailAttachments(XHTMLContentHandler xhtml, PSTMessage email,
-                                     final Metadata mailMetadata,
-                                      EmbeddedDocumentExtractor embeddedExtractor)
+                                      final Metadata mailMetadata,
+                                      EmbeddedDocumentExtractor embeddedExtractor,
+                                      final String messageId)
             throws TikaException {
         int numberOfAttachments = email.getNumberOfAttachments();
         for (int i = 0; i < numberOfAttachments; i++) {
